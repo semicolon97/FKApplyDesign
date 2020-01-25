3afc1eb15961eb4b4b0db0925af763d21448e3f8
Akash Mishra <12akashmishra@gmail.com>
2020-01-25 11:37:00 +0530

    Feature 03 : Game conditions implementation, board and players creation 	1. Created an ArrayList of Player class to store all players 	2. Created players of types Human and Machine based on user input 	3. Stored all created players in the above ArrayList and assigned them playerId TODO: 	1. Change game conditions if there are future requirements

--- Game.java
+++ Game.java
@@ -1,10 +1,13 @@
 package FKApplyDesign;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Game {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter Board Row Size");
         int rowSize = sc.nextInt();
         System.out.println("Enter Board Column Size");
@@ -16,5 +19,36 @@ public class Game {
         gameBoard.setCol_size(colSize);
 
         gameBoard.initializeBoard();
+
+        System.out.println("enter number of Players");
+        int noOfPlayers = sc.nextInt();
+
+        ArrayList<Player> listOfPlayers = new ArrayList<>(noOfPlayers);
+
+        System.out.println("For Each Player, Enter H for human and M for Machine");
+        int playerId = 0;
+        String choice;
+        Human tempHuman;
+        Machine tempMachine;
+
+        for (int iter=0;iter<noOfPlayers;iter++) {
+            choice = sc.nextLine();
+            if (choice.equals("H")) {
+                tempHuman = new Human();
+                tempHuman.setPlayerId(playerId);
+                playerId++;
+                listOfPlayers.add(tempHuman);
+            }
+            else if (choice.equals("M")) {
+                tempMachine = new Machine();
+                tempMachine.setPlayerId(playerId);
+                playerId++;
+                listOfPlayers.add(tempMachine);
+            }
+            else {
+                System.out.println("Invalid Choice, please enter again");
+                iter--;
+            }
+        }
     }
 }
