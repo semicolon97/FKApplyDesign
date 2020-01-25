7482c522dd4065fe707895df4779436efcdb55f6
Akash Mishra <12akashmishra@gmail.com>
2020-01-25 13:03:14 +0530

    Feature 04 : Simulate the game conditions and play the game 	1. Modified the diagonalCrossed method to fix a bug 	2. created ShowBoard method to disply the tic-tac-toe board after every move 	3. Modified how the winner is calculated to remove a bug

--- Game.java
+++ Game.java
@@ -82,6 +82,7 @@ public class Game {
         if (initialLoc != -1) {
             for (int i=0;i<rowSize;i++) {
                 if (gameBoard.getCell(rowSize-i-1,i)!=initialLoc) {
+                    flag = 0;
                     break;
                 }
             }
@@ -97,6 +98,19 @@ public class Game {
     boolean gameOver(Board gameBoard, int rowSize, int colSize) {
         return (rowCrossed(gameBoard, rowSize, colSize) || colCrossed(gameBoard,rowSize,colSize) || diagonalCrossed(gameBoard,rowSize,colSize));
     }
+
+    void showBoard(Board gameBoard) {
+        int rows = gameBoard.getRow_size();
+        int cols = gameBoard.getCol_size();
+
+        for (int i=0;i<rows;i++) {
+            for (int j=0;j<cols;j++) {
+                System.out.print(gameBoard.getCell(i,j)+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -163,13 +177,22 @@ public class Game {
                 moveY = whoMoves.colMove(colSize);
             }
             whoseTurn = (whoseTurn+1)%noOfPlayers;
+            whoMoves = listOfPlayers[whoseTurn];
+            noOfMovesdone++;
+
+            newGame.showBoard(gameBoard);
         }
 
         if (newGame.gameOver(gameBoard,rowSize,colSize) == false && noOfMovesdone == noOfValidMoves) {
             System.out.println("Game is a draw");
         }
         else {
-            whoseTurn = (whoseTurn - 1)%noOfPlayers;
+            if (whoseTurn == 0) {
+                whoseTurn = noOfPlayers - 1;
+            }
+            else {
+                whoseTurn = (whoseTurn - 1) % noOfPlayers;
+            }
             System.out.println("The Winner is Player "+whoseTurn);
         }
     }
