55086f7023685968e3b9d2aed1862445ef1771d6
Akash Mishra <12akashmishra@gmail.com>
2020-01-25 10:18:33 +0530

    Feature 01: Implement TicTacToe Board 	1. Created a 2-D Array to simulate a tictactoe board 	2. Created a method initializeBoard to initialize the above 	   array with some initial value(-1 in this case) TODO: 	1. Implement a method to make a move on the board 	2. Implement a method to check if the board is full

--- Board.java
+++ Board.java
@@ -3,6 +3,7 @@ package FKApplyDesign;
 public class Board {
     private int row_size;
     private int col_size;
+    private int[][] board = new int[row_size][col_size];
 
     void setRow_size(int n) {
         row_size = n;
@@ -16,5 +17,13 @@ public class Board {
     }
     int getCol_size() {
         return col_size;
+    }
+
+    void initializeBoard() {
+        for (int i=0;i<row_size;i++) {
+            for (int j=0;j<col_size;j++) {
+                board[i][j]=-1;
+            }
+        }
     }
 }
