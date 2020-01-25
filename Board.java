51b3b53535708a32422fbc8038683a6427d566d5
Akash Mishra <12akashmishra@gmail.com>
2020-01-25 13:13:41 +0530

    Feature 01 : TicTacToeGameImplementingSquares3x3 	1. Changed visibility of Board class from Public to Default 	2. Initialization of board attribute now being done in initializeboard method 	3. Messages now printed in case of invalid inputs 	4. getCell method created to return the value stored in the cell asked for by the user TODO: 	1. New features will be added as and when the requirements change

--- Board.java
+++ Board.java
@@ -1,9 +1,9 @@
 package FKApplyDesign;
 
-public class Board {
+class Board {
     private int row_size;
     private int col_size;
-    private int[][] board = new int[row_size][col_size];
+    private int[][] board;
 
     void setRow_size(int n) {
         row_size = n;
@@ -19,7 +19,17 @@ public class Board {
         return col_size;
     }
 
+    int getCell(int x, int y) {
+        if (x<0||x>=row_size||y<0||y>=col_size) {
+            return -1;
+        }
+        else {
+            return board[x][y];
+        }
+    }
+
     void initializeBoard() {
+        board = new int[row_size][col_size];
         for (int i=0;i<row_size;i++) {
             for (int j=0;j<col_size;j++) {
                 board[i][j]=-1;
@@ -29,9 +39,11 @@ public class Board {
 
     int makeMove(int player, int x, int y) {
         if (x<0||x>=row_size||y<0||y>=col_size) {
+            System.out.println("Invalid Move");
             return -1;
         }
         else if (board[x][y] != -1) {
+            System.out.println("Cell already occupied");
             return 0;
         }
         else {
