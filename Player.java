24d2af14878ef07a2f3a2459e7f35914d49c19c1
Akash Mishra <12akashmishra@gmail.com>
2020-01-25 10:53:04 +0530

    Feature 02 : Define Players who will play the game 	1. Created Human class that inherits from Player class 	2. Implemented rowMove and colMove methods TODO: 	1. Create Machine class 	2. Implement Machine class methods

--- Player.java
+++ Player.java
@@ -1,5 +1,7 @@
 package FKApplyDesign;
 
+import java.util.Scanner;
+
 public abstract class Player {
     private int playerId;
 
@@ -11,6 +13,24 @@ public abstract class Player {
         playerId=id;
     }
 
-    public abstract int rowMove();
-    public abstract int colMove();
+    abstract int rowMove();
+    abstract int colMove();
+}
+
+
+
+class Humans extends Player {
+    int rowMove() {
+        System.out.println("Enter row number of your move");
+        Scanner sc = new Scanner(System.in);
+        int rowNo = sc.nextInt();
+        return rowNo;
+    }
+
+    int colMove() {
+        System.out.println("Enter column number of your move");
+        Scanner sc = new Scanner(System.in);
+        int colNo = sc.nextInt();
+        return colNo;
+    }
 }
