4019d6056b6577ae57b890d023cb1d0a74945236
semicolon97 <12akashmishra@gmail.com>
2020-01-28 13:12:40 +0530

    Feature 06 : Implement Enhanced TicTacToe 	1. Implemented playEnhancedGame method to simulated enhanced tictactoe 	2. Implemented new methods for row, column and diagonal comparisons of intermediate depth boards 	3. The enhancedGame method will recursively determine the winner of each board, starting from nxn level 	   to the highest depth level. 	4. Edited the main method to offer a choice between regular and enhanced versions of the game 	5. Changed the return type of the playGame and playEnhancedGame methods to return the result in form of 	   playerId or -1 in case of a draw. TODO: 	1. Any future changes as and when needed.

--- Game.java
+++ Game.java
@@ -5,6 +5,97 @@ import java.util.ArrayList;
 
 public class Game {
 
+    boolean newRowCrossed(int [][] winners, int rowSize, int colSize) {
+        int initialLoc, flag = 1;
+        for (int i = 0 ; i < rowSize ; i++) {
+            flag = 1;
+            initialLoc = winners[i][0];
+            if (initialLoc == -1) {
+                flag = 0;
+                continue;
+            }
+            for (int j = 0 ; j < colSize ; j++) {
+                if (winners[i][j] != initialLoc) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag==1) {
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    boolean newColCrossed(int[][] winners, int rowSize, int colSize) {
+        int initialLoc, flag = 1;
+        for (int i = 0 ; i < colSize ; i++) {
+            flag = 1;
+            initialLoc = winners[0][i];
+            if (initialLoc == -1) {
+                flag = 0;
+                continue;
+            }
+            for (int j = 0 ; j < rowSize ; j++) {
+                if (winners[j][i] != initialLoc) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag==1) {
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    boolean newDiagonalCrossed(int[][] winners, int rowSize, int colSize) {
+        int initialLoc, flag = 1;
+        initialLoc = winners[0][0];
+
+        if (initialLoc != -1) {
+            for (int i=0;i<rowSize;i++) {
+                if (winners[i][i]!=initialLoc) {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+
+        if (initialLoc != -1 && flag == 1) {
+            return true;
+        }
+
+        flag = 1;
+        initialLoc = winners[rowSize-1][0];
+        if (initialLoc != -1) {
+            for (int i=0;i<rowSize;i++) {
+                if (winners[rowSize-i-1][i]!=initialLoc) {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+
+        if (initialLoc != -1 && flag == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
     boolean rowCrossed(Board gameBoard, int rowSize, int colSize) {
         int initialLoc, flag = 1;
         for (int i = 0 ; i < rowSize ; i++) {
@@ -111,7 +202,7 @@ public class Game {
         }
     }
 
-    void playGame( Board gameBoard, Player[] listOfPlayers, int rowSize, int colSize) {
+    int playGame( Board gameBoard, Player[] listOfPlayers, int rowSize, int colSize) {
         int noOfValidMoves = rowSize * colSize;
         int noOfMovesdone = 0;
         int whoseTurn = 0;
@@ -138,11 +229,13 @@ public class Game {
             whoMoves = listOfPlayers[whoseTurn];
             noOfMovesdone++;
 
-            showBoard(gameBoard);
+            //showBoard(gameBoard);
         }
 
         if (gameOver(gameBoard,rowSize,colSize) == false && noOfMovesdone == noOfValidMoves) {
             System.out.println("Game is a draw");
+            showBoard(gameBoard);
+            return -1;
         }
         else {
             if (whoseTurn == 0) {
@@ -152,9 +245,47 @@ public class Game {
                 whoseTurn = (whoseTurn - 1) % noOfPlayers;
             }
             System.out.println("The Winner is Player "+whoseTurn);
+            showBoard(gameBoard);
+            return whoseTurn;
         }
+
     }
 
+    int playEnhancedGame(Player[] listOfPlayers, int rowSize, int colSize, int boardSize) {
+        int tempWinner;
+
+        if (boardSize == rowSize) {
+
+            Board gameBoard = new Board();
+            gameBoard.setRow_size(rowSize);
+            gameBoard.setCol_size(colSize);
+
+            gameBoard.initializeBoard();
+
+            tempWinner = playGame(gameBoard, listOfPlayers, rowSize, colSize);
+
+            return tempWinner;
+        }
+        else {
+            int [][] winners = new int[rowSize][colSize];
+
+            for (int i=0;i<rowSize;i++) {
+                for (int j=0;j<colSize;j++) {
+                    winners[i][j]=-1;
+                }
+            }
+            for (int i=0;i<rowSize;i++) {
+                for (int j=0;j<colSize;j++) {
+                    winners[i][j] = playEnhancedGame(listOfPlayers, rowSize, colSize, boardSize/rowSize);
+                    if (newRowCrossed(winners, rowSize, colSize) || newColCrossed(winners, rowSize, colSize) || newDiagonalCrossed(winners, rowSize, colSize)) {
+                        return winners[i][j];
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -205,7 +336,25 @@ public class Game {
             }
         }
 
+        System.out.println("Enter Y to play enhanced Tictactoe");
+        choice = sc.nextLine();
 
-        newGame.playGame(gameBoard, listOfPlayers, rowSize, colSize );
+        int winner;
+
+        if (choice.equals("Y")) {
+            System.out.println("Enter the board size in terms of power of row or column size");
+            int boardSize = sc.nextInt();
+
+            winner = newGame.playEnhancedGame(listOfPlayers, rowSize, colSize, boardSize);
+            if (winner == -1) {
+                System.out.println("The Game is a draw");
+            }
+            else {
+                System.out.println("The Winner of the Enhanced TicTacToe is " +winner);
+            }
+        }
+        else {
+            winner = newGame.playGame(gameBoard, listOfPlayers, rowSize, colSize );
+        }
     }
 }
